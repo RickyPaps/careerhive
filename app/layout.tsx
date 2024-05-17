2538f6495b1e86d7669bcba987c822da67fa5d5e
Riccardo Papini <rickypapini@gmail.com>
2024-05-17 15:25:33 +0100

    Added clerk auth and added sign in button to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "CareerHive App",
@@ -13,18 +14,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className='min-h-screen flex flex-col'>
-        {/* Toaster */}
+    <ClerkProvider>
+      <html lang="en">
+        <body className="min-h-screen flex flex-col">
+          {/* Toaster */}
 
-        <header className="border-b sticky top-0 z-50 bg-black">
-          <Header />
-        </header>
+          <header className="border-b sticky top-0 z-50 bg-black">
+            <Header />
+          </header>
 
-        <div className="bg-[#F5F5F5] flex-1 w-full">
-          <main> {children}</main>
-        </div>
-      </body>
-    </html>
+          <div className="bg-[#F5F5F5] flex-1 w-full">
+            <main> {children}</main>
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }

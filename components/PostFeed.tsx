acb480f60a4f8e0eb5570e391dced08d98650624
Riccardo Papini <rickypapini@gmail.com>
2024-06-17 11:38:20 +0200

    Finished likes

--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Post from "./Post";
 
 const PostFeed = ({ posts }: { posts: IPostDocument[] }) => {
-  console.log(posts);
   return (
     <div className="space-y-2 pb-20">
       {posts.map((post: any) => (
